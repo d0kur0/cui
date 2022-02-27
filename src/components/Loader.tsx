51ce550d2c27bb1c10710d478f239aaaee5ed6ec
d0kur0 <dev.dokuro@gmail.com>
2022-02-27 07:01:34 +0300

    chore: add CircleRounded font, minor UI fixes

--- src/components/Loader.tsx
+++ src/components/Loader.tsx
@@ -3,7 +3,7 @@ import styles from "./Loader.module.css";
 function Loader() {
 	return (
 		<svg className={styles.spinner} viewBox="0 0 50 50">
-			<circle cx="25" cy="25" r="20" fill="none" stroke-width="7" />
+			<circle cx="25" cy="25" r="20" fill="none" stroke-width="4" />
 		</svg>
 	);
 }
--- src/pages/Events.tsx
+++ src/pages/Events.tsx
@@ -1,13 +1,22 @@
 import Layout from "../components/Layout";
 import NavBar from "../components/NavBar";
 import Title from "../components/Title";
-
 import Calendar from "../components/Calendar";
+import { FiPlusSquare } from "solid-icons/fi";
 
 function Events() {
 	return (
 		<Layout
-			title={<Title rightChildren={<button>Добавить</button>} title="Записи" />}
+			title={
+				<Title
+					rightChildren={
+						<button>
+							<FiPlusSquare size={28} />
+						</button>
+					}
+					title="Записи"
+				/>
+			}
 			navBar={<NavBar />}>
 			<Calendar />
 		</Layout>
