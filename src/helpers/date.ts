4c0476b3ababe4b85e65daa03d5ffad386e90436
d0kur0 <dev.dokuro@gmail.com>
2022-07-29 02:41:42 +0300

    chore: add DateRange component for statistic page

--- src/helpers/date.ts
+++ src/helpers/date.ts
@@ -1,10 +1,14 @@
 import { format as formatF, formatRelative as formatRelativeF } from "date-fns";
 import { ru } from "date-fns/locale";
 
-export function formatRelative(date: Date | undefined): string {
+export function formatRelative(date: Date | undefined) {
 	return formatRelativeF(date || new Date(), new Date(), { locale: ru });
 }
 
-export function format(date: Date): string {
+export function format(date: Date) {
 	return formatF(date, "d LLLL yyyy", { locale: ru });
+}
+
+export function formatForInput(date: Date | undefined, skipTime = false) {
+	return formatF(date || new Date(), `yyyy-MM-dd${skipTime ? "" : "'T'HH:mm"}`);
 }
--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,5 +1,6 @@
 import { Match, Switch, createEffect, createMemo, onMount } from "solid-js";
 
+import { DateRange } from "../components/DateRange";
 import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { Paper } from "../components/Paper";
@@ -15,12 +16,22 @@ export function Stats() {
 		statistics.records.length || setRecords(records.list);
 	});
 
+	const Statistic = () => {
+		return (
+			<>
+				<DateRange onChange={alert} start={statistics.range.start} end={statistics.range.end} />
+			</>
+		);
+	};
+
 	return (
 		<Layout title={<Title title="Статистика" />} navBar={<NavBar />}>
-			<Paper>
+			<Paper padding="15px">
 				<Switch>
 					<Match when={statistics.isLoading}>Загрузка...</Match>
-					<Match when={!statistics.isLoading}>статистика {statistics.records.length}</Match>
+					<Match when={!statistics.isLoading}>
+						<Statistic />
+					</Match>
 				</Switch>
 			</Paper>
 		</Layout>
