955184f652e324aee4577c63540ef0265cf88ab9
d0kur0 <dev.dokuro@gmail.com>
2022-07-29 01:20:13 +0300

    chore: first setup for statistic

--- src/pages/Stats.tsx
+++ src/pages/Stats.tsx
@@ -1,12 +1,28 @@
+import { Match, Switch, createEffect, createMemo, onMount } from "solid-js";
+
 import { Layout } from "../components/Layout";
 import { NavBar } from "../components/NavBar";
 import { Paper } from "../components/Paper";
 import { Title } from "../components/Title";
 
+import { useStore } from "../stores";
+
 export function Stats() {
+	const { statistics, setRecords } = useStore("statistics");
+	const { records } = useStore("records");
+
+	createEffect(() => {
+		statistics.records.length || setRecords(records.list);
+	});
+
 	return (
 		<Layout title={<Title title="Статистика" />} navBar={<NavBar />}>
-			<Paper>WIP</Paper>
+			<Paper>
+				<Switch>
+					<Match when={statistics.isLoading}>Загрузка...</Match>
+					<Match when={!statistics.isLoading}>статистика {statistics.records.length}</Match>
+				</Switch>
+			</Paper>
 		</Layout>
 	);
 }
--- src/stores/index.ts
+++ src/stores/index.ts
@@ -2,6 +2,7 @@ import { clientsStore } from "./clients";
 import { notificationsStore } from "./notifications";
 import { recordsStore } from "./records";
 import { servicesStore } from "./services";
+import { statisticsStore } from "./statistics";
 import { userStore } from "./userStore";
 
 export type StaticStoreProps = {
@@ -13,10 +14,10 @@ const stores = {
 	clients: clientsStore,
 	records: recordsStore,
 	services: servicesStore,
+	statistics: statisticsStore,
 	notifications: notificationsStore,
 };
 
 export function useStore<T extends keyof typeof stores>(store: T): typeof stores[T] {
-	if (!stores?.[store]) throw new Error(`useStore: store ${store} not found`);
 	return stores[store];
 }
